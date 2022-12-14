bc6bb595804d76f97321dda95405372c7daf479d
Ferney Arroyave Quintero <farroyave@fundaciongruposocial.co>
2022-12-14 15:37:40 -0500

    Prueba descarte de tags testng run test

---
+++ src/main/java/utils/ExcelIO.java
@@ -0,0 +1,35 @@
+package utils;
+
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.IOException;
+
+public class ExcelIO {
+
+    public XSSFWorkbook workBook;
+    public XSSFSheet sheet;
+
+    public ExcelIO(String excelName, String sheetName) throws IOException {
+        workBook = new XSSFWorkbook(System.getProperty("user.dir") + "\\test-data\\" + excelName);
+        sheet = workBook.getSheet(sheetName);
+    }
+
+    public int getRowCount() {
+        return sheet.getPhysicalNumberOfRows();
+    }
+
+    public int getColCount() {
+        return sheet.getRow(0).getPhysicalNumberOfCells();
+    }
+
+    public String getCellDataString(int rowNum, int colNum) {
+        sheet.getRow(rowNum).getCell(colNum).setCellType(CellType.STRING);
+        return sheet.getRow(rowNum).getCell(colNum).toString();
+    }
+
+    public double getCellDataNumeric(int rowNum, int colNum) {
+        return sheet.getRow(rowNum).getCell(colNum).getNumericCellValue();
+    }
+}
--- src/test/java/test/java/SimulateCreditTest.java
+++ src/test/java/test/java/SimulateCreditTest.java
@@ -5,14 +5,18 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pom.LoginSvpPage;
+import utils.ExcelDataProvider;
 
+import java.io.IOException;
 import java.net.URL;
 
 public class SimulateCreditTest {
 
     public RemoteWebDriver driver;
+    public ExcelDataProvider excelDataProvider;
     public LoginSvpPage loginSvpPage;
     public String gridURL = "http://standalone-chrome:4444/";
 
@@ -23,17 +27,18 @@ public class SimulateCreditTest {
         capabilities.setPlatform(Platform.LINUX);
         try {
             driver = new RemoteWebDriver(new URL(gridURL), capabilities);
+            driver.manage().window().maximize();
         } catch (Exception exception) {
             System.out.println("Exception message " + exception.getMessage());
         }
     }
 
-    @Test
-    public void firstTestCase() {
+    @Test(testName = "Simular crédito usuario PF2.0", dataProvider="credit_simulation_data")
+    public void firstTestCase(String args[]) {
         try {
             driver.get("https://pwpwebqaohs.cajalosandes.cl/mi-sucursal/SimuladorDeCreditoUnico");
             loginSvpPage = new LoginSvpPage(driver);
-            loginSvpPage.loginUser("55589143", "QA2022");
+            loginSvpPage.loginUser(args[0], args[1]);
         } catch (Exception exception) {
             System.out.println("Exception message " + exception.getMessage());
         }
@@ -41,7 +46,13 @@ public class SimulateCreditTest {
 
     @AfterTest
     public void closeBrowser() {
-        driver.close();
+        driver.quit();
         System.out.println("The driver has been closed.");
+    }
+
+    @DataProvider(name = "credit_simulation_data")
+    public Object[][] getDataCreditSimulation() throws IOException {
+        excelDataProvider = new ExcelDataProvider();
+        return excelDataProvider.getTestData("credit_simulation_data.xlsx", "PF2.0");
     }
 }
